dff7817844bd5dfa2f8bbd00dbca02fe5d433c76
Isaias Mendes <isaiaszmendes@gmail.com>
2023-01-27 23:50:56 -0300

    settings docker compose

--- docker-challenge/nginx-node/nodejs/src/index.js
+++ docker-challenge/nginx-node/nodejs/src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
   if(req.url === "/"){
-    createPerson()
+    // createPerson()
     res.writeHead(200, { 'Content-Type':'text/html'});
     res.end("<h1>Full Cycle Rocks!</h1>");
   }
